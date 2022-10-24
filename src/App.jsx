778c89c9d4265f6436b95e573e63acb2bf91389b
Travis Alstrand <travisalstrand@gmail.com>
2022-10-24 16:13:28 -0700

    Basic header started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className='bg-gray-900 h-screen'>
+      <Header />
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className='flex justify-end border-black border-b-2 bg-gray-700'>
+      <button className='px-6 py-2 m-1 rounded bg-gray-300 text-black hover:bg-black hover:text-white border-black border-2'>Sign Up</button>
+      <button className='px-6 py-2 m-1 rounded bg-gray-300 text-black hover:bg-black hover:text-white border-black border-2'>Sign In</button>
+    </div>
+  )
+}
+
+export default Header;
