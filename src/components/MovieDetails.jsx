ab66f67f335ac939739f68f4b0b45a05653ba3d4
Travis Alstrand <travisalstrand@gmail.com>
2022-10-24 17:26:05 -0700

    started working with dummy data

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,5 +1,9 @@
-const MovieDetail = () => {
-
+const MovieDetails = (props) => {
+  return (
+    <div className=''>
+      <img className='w-32 m-2' src={props.movie.Poster} alt="Movie Poster" />
+    </div>
+  )
 };
 
-export default MovieDetail;
+export default MovieDetails;
--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -1,5 +1,15 @@
-const MoviesContainer = (props) => {
+import MovieDetails from "./MovieDetails";
 
+const MoviesContainer = (props) => {
+  return (
+    <div className='flex'>
+      {props.movies.map((movie, index) => {
+        return (
+          <MovieDetails key={index} movie={movie} />
+        )
+      })}
+    </div>
+  )
 };
 
 export default MoviesContainer;
